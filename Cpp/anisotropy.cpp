49e4a1d88d4b5eeeeec4923de55736dca31edcc7
Evelyn Coronel <astrocronopio@hotmail.com>
2020-02-04 19:44:15 +0000

    Primer archivo con corrección de clima, verificado.

--- Cpp/anisotropy.cpp
+++ Cpp/anisotropy.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <sstream>
 #include <math.h>
+#include <vector>
 
 using namespace std;
 
@@ -45,7 +46,56 @@ float efficiency(float theta, float weather, float energy)
 }
 
 
-void rayleigh( float *a , float *b, float *sumaN, float *freq, unsigned long utci, unsigned long utcf)
+void exposure_weight(vector<long double> & vect, unsigned long utci, unsigned long utcf, float freq)
+{	std::vector<long double>xnhexhr(24);
+	std::vector<long double>rnhexhr(24);
+
+
+	unsigned long iutc, iutc0 = 1072915200;
+	float t,p,r,rav,xnhex, xnhex5;
+	int iw,ib, ihr;
+	string line;
+	float fas = freq/365.25;
+	long double x1,x2,x3;
+
+	ifstream myweather("../../Weather/utctprh.dat");
+
+	if(myweather.is_open())
+	{
+		while (!myweather.eof() ){			
+			getline(myweather,line);			
+			stringstream liness(line);	
+			liness>>iutc>>t>>p>>r>>rav>>xnhex>>xnhex5>> iw>>ib>>x1>>x2>>x3;
+
+			if (iutc < utci || iutc > utcf) continue;
+
+			if (iw<4 && ib>0.5)
+			{
+				x1=(float(iutc-iutc0)/3600.+31.4971*24/360)*fas;
+				ihr= int(x1)%24;
+				xnhexhr[ihr] += (3*xnhex5-xnhex)*0.5;
+
+				if (xnhexhr[ihr]>1) rnhexhr[ihr] += xnhexhr[ihr]; 
+			}
+
+		}
+	}
+
+	x2=0;
+	for (int i = 0; i < 24; ++i)
+	{	
+		rnhexhr[i]+=xnhexhr[i];
+		x2+=rnhexhr[i]/24.0;
+	}
+
+	
+	for (int i = 0; i < 24; ++i) {
+		vect[i] = rnhexhr[i]/x2;
+		//cout << vect[i] <<endl;
+		}
+}
+
+void rayleigh( float *a , float *b, float *sumaN, float freq, unsigned long utci, unsigned long utcf)
 {
 	float phitilt = -30.0*d2r;
 	unsigned long utc0 = 1072915200;
@@ -56,19 +106,24 @@ void rayleigh( float *a , float *b, float *sumaN, float *freq, unsigned long utc
 	float Phi,Theta,Ra,x1,x3,energy, p, r, rav, AugId,Dec,energy_raw, energy_cor,ftr;
 	float arho, aP, brho;
 
-	float fas = *freq/365.25;
+	float fas = freq/365.25;
 
-	ifstream myfile ("../../Herald/Central/Modified/Energy_above_1EeV/Herald_8EeV.dat");
-	//ifstream myfile ("../Herald080noBP5n6t5a4_pnop_04-310816_UncorCorE.dat");
+	//ifstream myfile ("../../Herald/Central/Modified/Energy_above_1EeV/Herald_8EeV.dat");
+	ifstream myfile ("../Herald080noBP5n6t5a4_pnop_04-310816_UncorCorE.dat");
+
+	vector<long double> dnhex(24);
+
+	exposure_weight(dnhex, utci, utcf, freq);
+
 
 	if(myfile.is_open())
 	{
 		while (!myfile.eof() ){			
 			getline(myfile,line);			
 			stringstream liness(line);			
-			liness >> utc>>Phi>>Theta>>Ra>>x1>>x3>>energy>>t5; //>> p>> r>> rav>> iw ;
-			//liness>> AugId >> Dec>> Ra>> energy_raw >> energy_cor >> utc >> Theta>>Phi>>t5>>ftr;
-			//energy=energy_raw;
+			//liness >> utc>>Phi>>Theta>>Ra>>x1>>x3>>energy>>t5; //>> p>> r>> rav>> iw ;
+			liness>> AugId >> Dec>> Ra>> energy_raw >> energy_cor >> utc >> Theta>>Phi>>t5>>ftr;
+			energy=energy_raw;
 			if(utc < utci || utc > utcf || Theta > 80 || energy<=8.0) continue;
 		
 			//weather_correction(Theta, &aP, &arho, &brho);
@@ -76,11 +131,10 @@ void rayleigh( float *a , float *b, float *sumaN, float *freq, unsigned long utc
 			//float eff= efficiency(Theta,weather, energy);
 
 			float  hrs= (float(utc - utc0)/3600.0 + 31.4971*24.0/360.0)*fas;
-			//int nh = (hrs%24);
+			int nh = (int(hrs)%24);
 
-			//float exposure= exposure_weight(utci, utcf, nh); 
+			float peso =1.0/dnhex[nh];
 
-			float peso =1.0;
 			*sumaN+=peso;
 			float raz = right_ascension(utc, utc0);
 
@@ -96,8 +150,8 @@ void rayleigh( float *a , float *b, float *sumaN, float *freq, unsigned long utc
 
 }
 
-float ray_multifreq(){
-	int nf=500;
+void ray_multifreq(int nf){
+	//int nf=50;
 	unsigned long utci = 1072915200;
 	unsigned long utcf = 1544933508;
 
@@ -105,15 +159,17 @@ float ray_multifreq(){
 	float rtilde,pha,prtilde,r99r;
 	float sigma=0.0, sgmra=0.0;
 
-	ofstream myfile ("ICRC2018_data_file_Eraw.txt");
+	ofstream myfile ("Tesis_data_file_Eraw.txt");
 
 	for (int i = 0; i < nf; ++i)
-	{
+	{	
+		cout << " Iteration "<< i +1 <<" of " << nf<< endl;
+
 		float freq = 364.0 + i*3.0/nf;
 
 		a=0.0; b=0.0; sumaN=0.0;
 
-		rayleigh(&a, &b, &sumaN, &freq, utci, utcf);
+		rayleigh(&a, &b, &sumaN, freq, utci, utcf);
 
 		a = 2.*a/sumaN;
      	b = 2.*b/sumaN;
@@ -134,17 +190,9 @@ float ray_multifreq(){
 }
 
 
-float exposure_weight(unsigned long utci, unsigned long utcf)
-{
-	
-
-	return 1;
-}
-
-
 int main(int argc, char const *argv[])
 {	
-	ray_multifreq();
+	ray_multifreq(500);
 	
 	return 0;
 }
